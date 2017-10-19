6f13b0b1093b6599f746f85f92704105d732bb28
cancerberosoft1 <crisjherico@gmail.com>
2017-10-19 10:15:03 -0500

    Add code to use spring profiles

--- src/main/java/com/jcczdev/web/services/PrimarySpanishGreetingService.java
+++ src/main/java/com/jcczdev/web/services/PrimarySpanishGreetingService.java
@@ -5,20 +5,20 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
- * @author Juan Carlos - 2017/10/17
+ * @author Juan Carlos - 2017/10/19
  *
  */
 @Service
+@Profile("es")
 @Primary
-@Profile("en")
-public class PrimaryGreetingService implements GreetingService {
+public class PrimarySpanishGreetingService implements GreetingService {
 
 	/* (non-Javadoc)
 	 * @see com.jcczdev.web.services.GreetingService#sayGreeting()
 	 */
 	@Override
 	public String sayGreeting() {
-		return "Hello- Primary Greeting Service";
+		return "Servicio de Saludo Primario";
 	}
 
 }
