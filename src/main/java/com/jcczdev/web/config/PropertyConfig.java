06f46beed903a1598afa8ccc1a9fad30a6b342d1
cancerberosoft1 <crisjherico@gmail.com>
2017-10-27 11:02:36 -0500

    Work with multiple properties files with new Spring 4 annotation

--- src/main/java/com/jcczdev/web/config/PropertyConfig.java
+++ src/main/java/com/jcczdev/web/config/PropertyConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
@@ -16,7 +17,11 @@ import com.jcczdev.web.examplebeans.FakeJmsBroker;
  *
  */
 @Configuration
-@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+@PropertySources({
+	@PropertySource("classpath:datasource.properties"),
+	@PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
 	
 	@Autowired
